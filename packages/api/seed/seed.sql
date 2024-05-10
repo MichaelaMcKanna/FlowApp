5bc28e32e187ec14d62245aa22e76475d7618364
Michaela Schafer <michaelaschafer@Michaelas-MBP.lan>
2024-05-10 07:30:27 -0500

    First T4 commit

---
+++ packages/api/seed/seed.sql
@@ -0,0 +1,25 @@
+INSERT INTO Actions (id, defaultActions)
+VALUES
+  (1, 'Bridging'),
+  (2, 'Frame and Hip Escape'),
+  (3, 'Shrimping'),
+  (4, 'Technical Stand Up'),
+  (5, 'Turtle Turnover');
+  
+INSERT INTO Positions (id, defaultPositions)
+VALUES
+  (1, 'Back Control'),
+  (2, 'Butterfly Guard'),
+  (3, 'Closed Guard'),
+  (4, 'Crucifix'),
+  (5, 'De La Riva Guard'),
+  (6, 'Full Mount'),
+  (7, 'Guard'),
+  (8, 'Half Guard'),
+  (9, 'Knee On Belly'),
+  (10, 'Open Guard'),
+  (11, 'Rear Mount'),
+  (12, 'Side Control'),
+  (13, 'Technical Mount'),
+  (14, 'Turtle'),
+  (15, 'X Guard');
